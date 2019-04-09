395b030b4d9b25b40496cdffb3cbfa4049fe0abc
panxl6 <sysupanxl6@gmail.com>
2019-04-09 08:57:33 +0800

    实现哈希表增删查改接口

--- Write-hashtable-in-php/HashTable.php
+++ Write-hashtable-in-php/HashTable.php
@@ -9,6 +9,7 @@ class HashTale
 
     public function __construct($size=1000)
     {
+        // 哈希表大小
         $this->size = $size;
         // 初始化容器
         $this->container = array_fill(0, $size, $this->getItemDefine());
@@ -16,17 +17,36 @@ class HashTale
     
     public function get($key)
     {
+        $hashNum = $this->hashNum($key);
+        $result = $this->container[$hashNum];
         
+        if ($result['key'] == $key) {
+            return $result['value'];
+        }
+        
+        return null;
     }
     
     public function set($key, $value)
     {
+        $hashNum = $this->hashNum($key);
         
+        $result = $this->container[$hashNum];
+        
+        $result['key'] = $key;
+        $result['value'] = $value;
+        
+        $this->container[$hashNum] = $result;
+
+        var_dump($this->container);
+        
+        return null;
     }
     
     public function delete($key)
     {
-        
+        $hashNum = $this->hashNum($key);
+        $this->container[$hashNum] = $this->getItemDefine();
     }
 
     private function getItemDefine()
@@ -58,4 +78,11 @@ class HashTale
 }
 
 $hashTable = new HashTale(2);
+$hashTable->set('hello', 'world');
+var_dump($hashTable);
+
+$hashTable->set('key', 'value');
+var_dump($hashTable);
+
+$hashTable->set('crash', 'value');
 var_dump($hashTable);
