4c5f253a8e7c794d18ec8d8c97f3b88e348f4f81
panxl6 <sysupanxl6@gmail.com>
2019-04-09 08:47:32 +0800

    定义哈希表的增删查改API

--- Write-hashtable-in-php/HashTable.php
+++ Write-hashtable-in-php/HashTable.php
@@ -2,47 +2,60 @@
 
 class HashTale
 {
-	private $size = 0;
-	private $container = null;
-	
-	private $bigPrime = 1861;
+    private $size = 0;
+    private $container = null;
+    
+    private $bigPrime = 1861;
 
-	public function __construct($size=1000)
-	{
-		// 哈希表的大小
-		$this->size = $size;
-		// 初始化容器
-		$this->container = array_fill(0, $size, $this->getItemDefine());
-	}
-
-	private function getItemDefine()
-	{
-		$item = array(
-			'key' => '',
-			'value' => '',
-		);
-		
-		return $item;
+    public function __construct($size=1000)
+    {
+        $this->size = $size;
+        // 初始化容器
+        $this->container = array_fill(0, $size, $this->getItemDefine());
     }
     
-    public function hashNum($key)
-	{
+    public function get($key)
+    {
+        
+    }
+    
+    public function set($key, $value)
+    {
+        
+    }
+    
+    public function delete($key)
+    {
+        
+    }
+
+    private function getItemDefine()
+    {
+        $item = array(
+            'key' => '',
+            'value' => '',
+        );
+        
+        return $item;
+    }
+    
+    private function hashNum($key)
+    {
         // $key不能太长，$this->bigPrime也不能太大，不然会引起整数溢出
 
-		$hashNum = 0;
-		$keyLen = strlen($key);
+        $hashNum = 0;
+        $keyLen = strlen($key);
         
         // 这里的**幂运算你也可以用位移来实现
-		for ($i=0; $i<$keyLen; $i++) {
-			$hashNum += ($this->bigPrime ** ($keyLen - ($i+1))) * ord($key[$i]);
-		}
-		
-		$hashNum = $hashNum % $this->size;
-		
-		return $hashNum;
-	}
+        for ($i=0; $i<$keyLen; $i++) {
+            $hashNum += ($this->bigPrime ** ($keyLen - ($i+1))) * ord($key[$i]);
+        }
+        
+        $hashNum = $hashNum % $this->size;
+        
+        return $hashNum;
+    }
 }
 
-$hashTable = new HashTale();
-
-var_dump($hashTable->hashNum('hello'));
+$hashTable = new HashTale(2);
+var_dump($hashTable);
