17e832f17e220967f39db25a4bd83dce4b22488a
xinglongPan <you@example.com>
2019-04-17 23:16:52 +0800

    解析http请求

--- Write-webserver/main.c
+++ Write-webserver/main.c
@@ -6,7 +6,10 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+void request(const int* sock);
 void response(const int* sock);
+char* get_content_by_url(const char* url);
+
 
 int main()
 {
@@ -32,21 +35,50 @@ int main()
     }
 
     struct sockaddr_in client_addr;
-
-    while (1) {
+    int count = 0;
+    while (count < 1) {
         socklen_t client_addr_size = sizeof(client_addr);
         int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
         if (client_sock == -1) {
             puts("响应失败");
         }
 
+        request(&client_sock);
         response(&client_sock);
+
+        count++;
     }
 
     close(server_sock);
 
     return 0;
 }
+
+void request(const int* sock)
+{
+    int client_sock = *sock;
+    int max_size = 1024;
+
+    char buffer[max_size];
+    char method[max_size];
+    char file_name[max_size];
+
+    read(client_sock, buffer, sizeof(buffer) - 1);
+    puts("请求头部");
+    puts(buffer);
+
+    char sample[] = "GET /index.html HTTP/1.1";
+    char *p = strtok(sample, " ");
+
+    // 解析HTTP请求方法
+    strcpy(method, strtok(buffer, " "));
+    // 解析HTTP请求路径
+    strcpy(file_name, strtok(NULL, " "));
+
+    puts("客户端请求的文件:");
+    puts(file_name);
+}
+
 
 void response(const int* sock)
 {
@@ -60,4 +92,9 @@ void response(const int* sock)
     write(client_sock, content, strlen(content));
 
     close(client_sock);
+}
+
+char* get_content_by_url(const char* url)
+{
+    
 }
